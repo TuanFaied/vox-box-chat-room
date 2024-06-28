1640e95cb58f04879e9f0084ceccb6d39cbe718d
TuanFaied <tuanfaied8@gmail.com>
2024-06-29 00:08:54 +0530

    flutter install

--- vox_box_chat_room/android/app/src/main/kotlin/com/example/vox_box_chat_room/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.vox_box_chat_room
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ voxbox/android/app/src/main/kotlin/com/example/voxbox/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.voxbox
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
